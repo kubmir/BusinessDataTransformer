788941be2164c8f124f3a3f803fee0f764e4733f
Miroslav Kubus <433645@mail.muni.cz>
2021-12-28 22:35:05 +0100

    feat(analyzer): fe vs re per section analyze

--- BusinessDataAnalyzer/panelRegression/perSectionAnalyze.py
+++ BusinessDataAnalyzer/panelRegression/perSectionAnalyze.py
@@ -1,8 +1,13 @@
 import os
 import pandas as pd
 
+from linearmodels import PanelOLS
 from linearmodels import RandomEffects
 import statsmodels.api as sm
+
+from scipy import stats
+import numpy as np
+import numpy.linalg as la
 
 from filters import filterOutLayers, filterCompaniesWithOneRecord
 
@@ -11,16 +16,38 @@ def baseTestOwnerEffect(df, foreignOwnerColumnName, ownershipConcentrationColumn
     exog = sm.add_constant(df[exog_vars])
     endog = df[performanceVariable]
 
-    print(df)
     # random effects model
     model_re = RandomEffects(endog, exog, check_rank=False) 
     re_res = model_re.fit() 
 
-    # print results
-    print(re_res)
+    # fixed effects model
+    model_fe = PanelOLS(endog, exog, entity_effects = True, time_effects=True) 
+    fe_res = model_fe.fit() 
+
+    hausman_results = hausman(fe_res, re_res)
+
+    print("Hausmann p-Value: {}".format(str(hausman_results[2])))
+
+    if hausman_results[2] < 0.05:
+        print(fe_res)
+    else:
+        print(re_res)
+
 
 def filterSectionCompanies(df, currentSection):
     return df[df["Sekcia"] == currentSection]
+
+def hausman(fe, re):
+    b = fe.params
+    B = re.params
+    v_b = fe.cov
+    v_B = re.cov
+
+    df = b[np.abs(b) < 1e8].size
+    chi2 = np.dot((b - B).T, la.inv(v_b - v_B).dot(b - B))
+    pval = stats.chi2.sf(chi2, df)
+    
+    return chi2, df, pval
 
 def main(performanceVariable, sectionValue):
     col_list = [
