09bca279e81c563be7a343e93058288750499dec
Miroslav Kubus <433645@mail.muni.cz>
2021-12-28 22:03:36 +0100

    feat(analyzer): remove not used tests

--- BusinessDataAnalyzer/panelRegression/onePersonSroRegressionROA.py
+++
@@ -1,7 +0,0 @@
-from base import baseTestOwnerEffect
-
-def main():
-    baseTestOwnerEffect("ROA", "Jednoosobova_SRO")
-
-if __name__ == "__main__":
-    main()
--- BusinessDataAnalyzer/panelRegression/ownerConcentrationRegressionROE.py
+++
@@ -1,7 +0,0 @@
-from base import baseTestOwnerEffect
-
-def main():
-    baseTestOwnerEffect("ROE", "Koncentracia_vlastnictva")
-
-if __name__ == "__main__":
-    main()
